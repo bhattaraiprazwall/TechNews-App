b114d57b566a22f5a3d6271a2933ae30493194c9
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-04-08 23:09:31 +0545

    Written function to fetch news , Created ui of searchbox

---
+++ lib/backend/functions.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:technewsapp/components/searchbox.dart';
+import 'package:technewsapp/utils/key.dart';
+
+Future<List> fetchNews() async {
+  final response = await http.get(
+    Uri.parse(
+      'https://newsdata.io/api/1/latest?apikey='+Key.key+'&q='+Searchbox.searchController.text,
+    ),
+  );
+  Map result = jsonDecode(response.body);
+  print('Fetched');
+  return result['articles'];
+}
