36188061a5693e3af7015faf41abdfd18f719669
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-04-10 23:03:37 +0545

    Updated fetch function with try catch

--- lib/backend/functions.dart
+++ lib/backend/functions.dart
@@ -1,15 +1,19 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-import 'package:technewsapp/components/searchbox.dart';
 import 'package:technewsapp/utils/key.dart';
 
-Future<List> fetchNews() async {
-  final response = await http.get(
-    Uri.parse(
-      'https://newsdata.io/api/1/latest?size=10&apikey=${Key.key}&q='+Searchbox.searchController.text,
-    ),
-  );
-  Map result = jsonDecode(response.body);  print('Fetched');
-  return result['articles'];
+Future<List> fetchNews(String query) async {
+  try {
+    final response = await http.get(
+      Uri.parse(
+        'https://newsdata.io/api/1/latest?size=10&apikey=${access_Key.access_key}&q=$query',
+      ),
+    );
+    Map result = jsonDecode(response.body);
+    print('Fetched');
+    return result['articles'];
+  } catch (e) {
+    print('Unexpected Error $e');
+    return [];
+  }
 }
